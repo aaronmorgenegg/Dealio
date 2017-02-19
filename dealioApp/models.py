7f2329744ea3c092203cf8a51a909a2adc08fee1
Thomas Rizzo <thomas.rizzo96@gmail.com>
2017-02-19 14:05:20 -0700

    added yelp review member variable

--- dealioApp/models.py
+++ dealioApp/models.py
@@ -15,13 +15,16 @@ class Promotion(models.Model):
 class Restaurant(models.Model):
     name = models.CharField(max_length=25, unique=True)
     proms = models.ManyToManyField(Promotion)
+    review_link = models.CharField(max_length=50, unique=True)
 
     def __str__(self):
         return self.name
+
+    def get_review_link(self):
+        return self.review_link
 
     def getPromotions(self):
         promos = []
         for i in range(0,self.proms.count()):
             promos.append(self.proms.all()[i])
-
         return promos
